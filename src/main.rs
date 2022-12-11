381f32a3aebcaf36cc89977fb21c0330dff4a061
René Mayrhofer <rene@mayrhofer.eu.org>
2022-12-11 17:53:22 +0100

    Use FileBuffer instead of epxlicit looping over files to read them - this is memory mapped and is supposed to be faster.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
 extern crate crypto;
-//extern crate filebuffer;
 extern crate walkdir;
+extern crate filebuffer;
 
-// naive version
-use std::fs;
-use std::io;
-use std::io::BufRead;
-// end naive version
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
-//use filebuffer::FileBuffer;
+use filebuffer::FileBuffer;
 use walkdir::WalkDir;
 
 fn main() {
@@ -19,31 +14,12 @@ fn main() {
             continue;
         }
         
-        let mut sha = Sha256::new();
-        // TODO: fixme for more efficient stream handling
-//        let fbuffer = FileBuffer::open(&fname).expect("failed to open file");
-//        sha.input(&fbuffer);
+        let mut hasher = Sha256::new();
+        let fbuf = FileBuffer::open(&fname).expect(&format!("failed to open file {}", fname.display()).to_string());
+        hasher.input(&fbuf);
 
-// naive version
-        let file = fs::File::open(&fname).expect("failed to open file");
-        let mut reader = io::BufReader::new(file);
-
-        loop {
-            let consumed_len = {
-                let buffer = reader.fill_buf().expect("failed to read from file");
-                if buffer.len() == 0 {
-                    // End of file.
-                    break;
-                }
-                sha.input(buffer);
-                buffer.len()
-            };
-            reader.consume(consumed_len);
-        }
-// end naive version
-        
         // TODO: include hash-of-hashes for (sorted) files in directory
 
-        println!("{}: {}", sha.result_str(), fname.display());
+        println!("{}: {}", hasher.result_str(), fname.display());
     }
 }
