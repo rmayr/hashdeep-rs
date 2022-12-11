50907201a3f64eac77453d256077e115d882513d
René Mayrhofer <rene@mayrhofer.eu.org>
2022-12-11 19:14:51 +0100

    Use clap to parse arguments - unfortunately, this doubles binary size.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,44 @@
 extern crate crypto;
+//extern crate blake3;
 extern crate walkdir;
 extern crate filebuffer;
+extern crate clap;
 
 use crypto::digest::Digest;
 use crypto::sha2::Sha256;
 use filebuffer::FileBuffer;
 use walkdir::WalkDir;
+use clap::Parser;
+
+#[derive(Parser)]
+// TODO: why isn't this read from Cargo.toml?
+//#[command(author, version, about, long_about = None)]
+#[command(author = "René Mayrhofer", version, about = "Recursively compute file hashes over a tree", long_about = None)]
+struct Args {
+    /// Directory path to start recursive traversal from
+    #[arg(short, long, default_value_t = String::from("."))]
+    path: String,
+ 
+    /// Hash function: sha256 or blake3
+    #[arg(short = 'c', long, default_value_t = String::from("blake3"))]
+    hasher: String,
+ }
 
 fn main() {
-    for entry in WalkDir::new(".").sort_by_file_name().into_iter().filter_map(|e| e.ok()) {
+    let args = Args::parse();
+
+    for entry in WalkDir::new(args.path).sort_by_file_name().into_iter().filter_map(|e| e.ok()) {
         let fname = entry.path();
         if ! fname.is_file() {
             continue;
         }
         
-        let mut hasher = Sha256::new();
         let fbuf = FileBuffer::open(&fname).expect(&format!("failed to open file {}", fname.display()).to_string());
+        let mut hasher = /*match args.hasher  {
+            "sha256" => */ Sha256::new() /*,
+            "blake3" => blake3::Hasher::new(),
+            _ => panic!("unknown hash function"),
+        } */ ;
         hasher.input(&fbuf);
 
         // TODO: include hash-of-hashes for (sorted) files in directory
