5c0066c974a59da8e832a4015a0ddc24d66dac4e
René Mayrhofer <rene@mayrhofer.eu.org>
2024-02-12 20:59:11 +0100

    Start trying to support other hash primitives in addition to Blake3 to become more compatible with other standard tools for comparing single files. However, this doesn't compile because Rust dyn traits are complicated...

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-//extern crate crypto;
-extern crate blake3;
+extern crate digest;
 extern crate walkdir;
 extern crate filebuffer;
 extern crate clap;
 
-/*use crypto::digest::Digest;
-use crypto::sha2::Sha256;*/
+#[cfg(feature = "sha2")]
+extern crate sha2;
+
+#[cfg(feature = "sha3")]
+extern crate sha3;
+
+#[cfg(feature = "blake3")]
+extern crate blake3;
+
+use digest::Digest;
+use digest::crypto_common::generic_array::{GenericArray, ArrayLength};
+use digest::typenum::U32;
 use filebuffer::FileBuffer;
 use walkdir::WalkDir;
 use clap::Parser;
@@ -19,19 +28,83 @@ struct Args {
     #[arg(short, long, default_value_t = String::from("."))]
     path: String,
  
-    /// Hash function: sha256 or blake3
+    /// Hash function: sha2, sha3, or blake3
     #[arg(short = 'c', long, default_value_t = String::from("blake3"))]
     hasher: String,
  }
 
+// Need to implement a local type for implementing the foreign traits
+/*pub struct Blake3Wrapper {
+    hasher: blake3::Hasher,
+}
+impl OutputSizeUser for Blake3Wrapper { type OutputSize = u32<32>; }
+
+impl Digest for Blake3Wrapper {
+    fn new() -> Self {
+        blake3::Hash
+        Blake3Wrapper {
+            hasher: blake3::Hasher::new()
+        }
+    }
+
+    fn new_with_prefix(data: impl AsRef<[u8]>) -> Self {
+        todo!()
+    }
+
+    fn update(&mut self, data: impl AsRef<[u8]>) {
+        self.hasher.update(data);
+    }
+
+    fn chain_update(self, data: impl AsRef<[u8]>) -> Self {
+        let mut h2 = self;
+        h2.update(data);
+        h2
+    }
+
+    fn finalize(self) -> Output<Self> {
+        self.hasher.finalize().into()
+    }
+
+    fn finalize_into(self, out: &mut Output<Self>) {
+        todo!()
+    }
+
+    fn finalize_reset(&mut self) -> Output<Self> where Self: FixedOutputReset {
+        todo!()
+    }
+
+    fn finalize_into_reset(&mut self, out: &mut Output<Self>) where Self: FixedOutputReset {
+        todo!()
+    }
+
+    fn reset(&mut self) where Self: Reset {
+        self.hasher.reset();
+    }
+
+    fn output_size() -> usize {
+        todo!()
+    }
+
+    fn digest(data: impl AsRef<[u8]>) -> Output<Self> {
+        todo!()
+    }
+}*/
+
 fn main() {
     let args = Args::parse();
 
-    let mut hasher = /*match args.hasher {
-        "sha256" => Sha256::new() ,
-        "blake3" => */ blake3::Hasher::new() /*,
+    let mut digest: Box<dyn Digest<OutputSize=GenericArray<u8, U32>>> = match args.hasher.as_str() {
+        #[cfg(feature = "sha2")]
+        "sha2" => Box::new(sha2::Sha256::new()),
+
+        #[cfg(feature = "sha3")]
+        "sha3" => Box::new(sha3::Sha3_256::new()),
+
+        #[cfg(feature = "blake3")]
+        "blake3" => Box::new(blake3::Hasher::new()),
+
         _ => panic!("unknown hash function")
-    }*/;
+    };
 
     for entry in WalkDir::new(args.path).sort_by_file_name().into_iter().filter_map(|e| e.ok()) {
         let fname = entry.path();
@@ -40,16 +113,12 @@ fn main() {
         }
         
         let fbuf = FileBuffer::open(&fname).expect(&format!("failed to open file {}", fname.display()).to_string());
-        /*let mut hasher = match args.hasher  {
-            "sha256" => Sha256::new(),
-            "blake3" => blake3::Hasher::new(),
-            _ => panic!("unknown hash function"),
-        } */ ;
-        hasher.reset();
-        hasher.update(&fbuf);
+        digest.reset();
+        digest.update(&fbuf);
 
+        let fhash = digest.finalize();
         // TODO: include hash-of-hashes for (sorted) files in directory
 
-        println!("{}: {}", hasher.finalize().to_hex(), fname.display());
+        println!("{}: {}", fhash.to_hex(), fname.display());
     }
 }
