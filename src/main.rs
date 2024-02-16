8a73a42dad3286d3402b5babbbe40dfde2e4f523
René Mayrhofer <rene@mayrhofer.eu.org>
2024-02-16 19:31:35 +0100

    Change from old OOP dynamic polymorphism pattern style to using traits with a generic method for switching between hash functions at run time. This compiles and works as intended, and is more the 'Rust way'.

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,6 @@ extern crate sha3;
 extern crate blake3;
 
 use digest::Digest;
-use digest::crypto_common::generic_array::{GenericArray, ArrayLength};
-use digest::typenum::U32;
 use filebuffer::FileBuffer;
 use walkdir::WalkDir;
 use clap::Parser;
@@ -33,92 +31,41 @@ struct Args {
     hasher: String,
  }
 
-// Need to implement a local type for implementing the foreign traits
-/*pub struct Blake3Wrapper {
-    hasher: blake3::Hasher,
-}
-impl OutputSizeUser for Blake3Wrapper { type OutputSize = u32<32>; }
-
-impl Digest for Blake3Wrapper {
-    fn new() -> Self {
-        blake3::Hash
-        Blake3Wrapper {
-            hasher: blake3::Hasher::new()
-        }
-    }
-
-    fn new_with_prefix(data: impl AsRef<[u8]>) -> Self {
-        todo!()
-    }
-
-    fn update(&mut self, data: impl AsRef<[u8]>) {
-        self.hasher.update(data);
-    }
-
-    fn chain_update(self, data: impl AsRef<[u8]>) -> Self {
-        let mut h2 = self;
-        h2.update(data);
-        h2
-    }
-
-    fn finalize(self) -> Output<Self> {
-        self.hasher.finalize().into()
-    }
-
-    fn finalize_into(self, out: &mut Output<Self>) {
-        todo!()
-    }
-
-    fn finalize_reset(&mut self) -> Output<Self> where Self: FixedOutputReset {
-        todo!()
-    }
-
-    fn finalize_into_reset(&mut self, out: &mut Output<Self>) where Self: FixedOutputReset {
-        todo!()
-    }
-
-    fn reset(&mut self) where Self: Reset {
-        self.hasher.reset();
-    }
-
-    fn output_size() -> usize {
-        todo!()
-    }
-
-    fn digest(data: impl AsRef<[u8]>) -> Output<Self> {
-        todo!()
-    }
-}*/
-
 fn main() {
     let args = Args::parse();
 
-    let mut digest: Box<dyn Digest<OutputSize=GenericArray<u8, U32>>> = match args.hasher.as_str() {
+    match args.hasher.as_str() {
         #[cfg(feature = "sha2")]
-        "sha2" => Box::new(sha2::Sha256::new()),
+        "sha2" => scan_dir::<sha2::Sha256>(&args.path),
 
         #[cfg(feature = "sha3")]
-        "sha3" => Box::new(sha3::Sha3_256::new()),
+        "sha3" => scan_dir::<sha3::Sha3_256>(&args.path),
 
         #[cfg(feature = "blake3")]
-        "blake3" => Box::new(blake3::Hasher::new()),
+        "blake3" => scan_dir::<blake3::Hasher>(&args.path),
 
         _ => panic!("unknown hash function")
     };
+}
 
-    for entry in WalkDir::new(args.path).sort_by_file_name().into_iter().filter_map(|e| e.ok()) {
+fn scan_dir<D: Digest>(path: &str)
+        where D::OutputSize: std::ops::Add,
+              <D::OutputSize as std::ops::Add>::Output: digest::generic_array::ArrayLength<u8> {
+
+    for entry in WalkDir::new(path).sort_by_file_name().into_iter().filter_map(|e| e.ok()) {
         let fname = entry.path();
         if ! fname.is_file() {
             continue;
         }
-        
+
         let fbuf = FileBuffer::open(&fname).expect(&format!("failed to open file {}", fname.display()).to_string());
-        digest.reset();
+
+        // this is not optimal from a performance point of view, but reset is problamatic with re-using the digest in the generic case
+        let mut digest = D::new();
+        //Digest::reset(&mut digest);
         digest.update(&fbuf);
 
         let fhash = digest.finalize();
-        // TODO: include hash-of-hashes for (sorted) files in directory
-
-        println!("{}: {}", fhash.to_hex(), fname.display());
+        println!("{:x}: {}", fhash, fname.display());
     }
 }
