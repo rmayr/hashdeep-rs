df69b70d2b4546a50380a83faa8e4e4c6f19b6d9
René Mayrhofer <rene@mayrhofer.eu.org>
2024-03-16 22:30:09 +0100

    Start implementing audit mode

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ extern crate sha3;
 #[cfg(feature = "blake3")]
 extern crate blake3;
 
+use std::collections::HashMap;
+use std::io::BufRead;
 use digest::Digest;
 use filebuffer::FileBuffer;
 use walkdir::WalkDir;
@@ -23,16 +25,31 @@ use clap::Parser;
 #[command(author = "René Mayrhofer", version, about = "Recursively compute file hashes over a tree", long_about = None)]
 struct Args {
     /// Directory path to start recursive traversal from
-    #[arg(short, long, default_value_t = String::from("."))]
+    #[arg(short = 'p', long, default_value_t = String::from("."))]
     path: String,
  
     /// Hash function: sha2, sha3, or blake3
     #[arg(short = 'c', long, default_value_t = String::from("blake3"))]
     hasher: String,
+
+    /// Auditing mode: read hashes from file specific by this parameter and compare with files in path.
+    #[arg(short = 'a', long)]
+    audit: Option<String>,
  }
 
 fn main() {
     let args = Args::parse();
+
+    if args.audit.is_some() {
+        let audit_file = args.audit.unwrap();
+        println!("Reading hashes from {}", audit_file);
+        let faudit = FileBuffer::open(&audit_file).expect(&format!("failed to open file {}", audit_file).to_string());
+        /*let audit_map = ["hash", "path"].into_iter()
+            .zip(faudit.lines().map(|l| l.expect("Unable to read line {}").split_once(char::is_whitespace)))
+            .collect::<HashMap<_, _>>();
+        println!("{:#?}", audit_map);*/
+        panic!("not fully implemented")
+    }
 
     match args.hasher.as_str() {
         #[cfg(feature = "sha2")]
